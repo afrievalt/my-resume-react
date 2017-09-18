769e08f99823b4a65a1a6db6dc81d9523719aeef
Andy Frievalt <afrievalt@yahoo.com>
2017-09-17 21:23:03 -0500

    improve code readability

--- src/ducks/select-employment-history.js
+++ src/ducks/select-employment-history.js
@@ -1,39 +1,43 @@
-
-
-
 export const getAllIds = ({employmentHistory_byId, sortBy, filter}) => {
-  const sortByKeyFunction = (key1, key2) => 
-      (employmentHistory_byId[key1][sortBy] < employmentHistory_byId[key2][sortBy]) 
-        ? 1 
-        : -1 //note, this sort assumes no equal values
-  
-  const regEx = new RegExp(`(${filter})`, 'gi')
-  const nameTitleFilterFunction = key => 
-    !filter //true if no filter 
-    || employmentHistory_byId[key].name.match(regEx)
-    || employmentHistory_byId[key].title.match(regEx)
-    || employmentHistory_byId[key].skills.match(regEx)
-  
-  const acceding = sortBy !== 'endDate' 
-  const keys = Object.keys(employmentHistory_byId)
+	const keys = Object.keys(employmentHistory_byId)
 
-  const result = keys
-    .filter(nameTitleFilterFunction)
-    .sort(sortByKeyFunction)
-  return acceding ? result.reverse() : result
+	return keys
+		.filter(nameTitleFilterFunction(employmentHistory_byId, filter))
+		.sort(sortByKeyFunction(employmentHistory_byId, sortBy))
 }
 
+const nameTitleFilterFunction = (employmentHistory_byId, filter) =>
+{
+	const regEx = new RegExp(`(${filter})`, "gi")
+  
+	return key => 
+		!filter //true if no filter 
+				|| employmentHistory_byId[key].name.match(regEx)
+				|| employmentHistory_byId[key].title.match(regEx)
+				|| employmentHistory_byId[key].skills.match(regEx)
+}
+const sortByKeyFunction = (employmentHistory_byId, sortBy) =>
+	(key1, key2) => {
+		const value1 = employmentHistory_byId[key1][sortBy]
+		const value2 = employmentHistory_byId[key2][sortBy]
+		const accedingResult = value1 > value2 ? 1 : -1 
+		const isDescending = sortBy === "endDate" 
+		return isDescending ? accedingResult * -1 : accedingResult
+	}
+
 export const getEmploymentHistoryList = state => {
-  const {employmentHistory_byId} = state
-  const mapRow = rowId => {
-    const {dates: secondaryText, title, name, img} = employmentHistory_byId[rowId]
-    const primaryText = `${name} - ${title}`
-    return {
-      key: rowId,
-      primaryText,
-      secondaryText,
-      img  
-    }
-  }
-  return getAllIds(state).map(mapRow)
+	const {employmentHistory_byId} = state
+	const mapRow = rowId => {
+		const {dates: secondaryText, title, name, img} = employmentHistory_byId[rowId]
+		const primaryText = `${name} - ${title}`
+    
+		return {
+			key: rowId,
+			primaryText,
+			secondaryText,
+			img  
+		}
+	}
+
+	return getAllIds(state).map(mapRow)
 }
